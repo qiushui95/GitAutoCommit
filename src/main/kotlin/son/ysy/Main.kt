d0f83e920656d14052e431cd4c0d078e181a9662
YCX-COMPANY <974577817@qq.com>
2020-09-22 09:36:17 +0800

    2020-09-22 09:36:17:017

--- src/main/kotlin/son/ysy/Main.kt
+++ src/main/kotlin/son/ysy/Main.kt
@@ -3,13 +3,27 @@ package son.ysy
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.internal.storage.file.FileRepository
 import org.joda.time.DateTime
+import java.io.File
 
 fun main() {
-    val fileRepository = FileRepository(".git")
+    val gitDir = File(".git")
+    if (!gitDir.exists()) {
+        exit("不是git仓库!")
+        return
+    }
+    val fileRepository = FileRepository(File(gitDir.absolutePath))
     val git = Git(fileRepository)
+    if (git.status().call().isClean) {
+        exit("文件无改动")
+        return
+    }
     git.add().addFilepattern(".").call()
     git.commit().setMessage(DateTime.now().toString("yyyy-MM-dd HH:mm:ss:sss")).call()
     git.push().setPushAll().call()
-    println("任意键结束!")
+    exit("任意键结束!")
+}
+
+private fun exit(message: String) {
+    println(message)
     System.`in`.read()
 }
